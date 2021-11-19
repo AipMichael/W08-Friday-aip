0497f297875255bd518e62e57352c6f4e9d54898
AipBorges <mpbds@uic.es>
2021-11-19 14:56:42 +0100

    added robot class with reset method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-let a: number = 3;
+type RobotType = "Cleaner" | "Waiter" | "Developer";
+
+const getSuperName = (): string => {
+  const randomChars: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const randomNumbers: string = "0123456789";
+  let newName = "";
+  for (let i = 0; i < 3; i++) {
+    newName += randomChars.charAt(
+      Math.floor(Math.random() * randomChars.length)
+    );
+  }
+  for (let i = 0; i < 2; i++) {
+    newName += randomChars.charAt(
+      Math.floor(Math.random() * randomNumbers.length)
+    );
+  }
+  return newName;
+};
+class Robot {
+  name: string;
+  batery: number = 100;
+  type;
+
+  constructor(type: RobotType) {
+    this.name = getSuperName();
+    this.type = type;
+  }
+
+  reset() {
+    this.name = getSuperName();
+    this.batery = 100;
+  }
+}
